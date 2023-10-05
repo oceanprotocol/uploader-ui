6647306d389e7d5c165baf25fb60781ee2fb2457
Enzo Vezzaro <enzo-vezzaro@live.it>
2023-10-05 15:50:52 +0200

    import @oceanprotocol/uploader-ui-lib dynamically for ES module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 'use client'
+import dynamic from 'next/dynamic';
 import Image from 'next/image'
 import styles from './page.module.css'
-import { DBSUploader } from '@oceanprotocol/dbs-ui-lib';
-import '@oceanprotocol/dbs-ui-lib/dist/index.esm.css';
+import '@oceanprotocol/uploader-ui-lib/dist/index.es.css';
+const Uploader = dynamic(() => import('@oceanprotocol/uploader-ui-lib').then((module) => module.DBSUploader), { ssr: false });
 
 export default function Home() {
   return (
@@ -18,12 +19,12 @@ export default function Home() {
         />
       </div>
 
-      <DBSUploader 
+      <Uploader
         dbs_url={process.env.DBS_URL || 'https://dbs.oceanprotocol.com'}
         dbs_account={process.env.DBS_ACCOUNT || '0x5F8396D1BfDa5259Ee89196F892E4401BF3B596d'}
         infuraId={process.env.INFURA_ID || ''}
         walletConnectProjectId={process.env.WALLET_CONNECT_PROJECT_ID || ''}
-      />
+      /> 
 
       <div className={styles.center}>
         <a
