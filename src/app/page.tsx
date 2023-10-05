d789c75ec86b43ba537dba507ddff2137cd9f170
Enzo Vezzaro <enzo-vezzaro@live.it>
2023-10-05 11:52:05 +0200

    change dbs-ui to uploader-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
-import { DBSUploader } from '@oceanprotocol/dbs-ui-lib';
-import '@oceanprotocol/dbs-ui-lib/dist/index.esm.css';
+import { DBSUploader } from '@oceanprotocol/uploader-ui-lib'; 
+import '@oceanprotocol/uploader-ui-lib/dist/index.es.css';
 
 export default function Home() {
   return (
@@ -18,12 +18,12 @@ export default function Home() {
         />
       </div>
 
-      <DBSUploader 
+      <DBSUploader
         dbs_url={process.env.DBS_URL || 'https://dbs.oceanprotocol.com'}
         dbs_account={process.env.DBS_ACCOUNT || '0x5F8396D1BfDa5259Ee89196F892E4401BF3B596d'}
         infuraId={process.env.INFURA_ID || ''}
         walletConnectProjectId={process.env.WALLET_CONNECT_PROJECT_ID || ''}
-      />
+      /> 
 
       <div className={styles.center}>
         <a
