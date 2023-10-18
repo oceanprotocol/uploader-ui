ce72a6afe74435954b83f0ed1153f255a41b492a
Enzo Vezzaro <enzo-vezzaro@live.it>
2023-10-18 09:30:31 +0200

    connecting magiclink to wagmi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,27 +6,69 @@ import {
   ConnectKitButton
 } from 'connectkit'
 
-import { WagmiConfig, createConfig } from 'wagmi'
+import { WagmiConfig, createConfig, configureChains } from 'wagmi'
 import { polygon } from 'wagmi/chains'
 import styles from './page.module.css'
 import Layout from '../components/molecules/Layout';
+import { DedicatedWalletConnector, UniversalWalletConnector } from '@magiclabs/wagmi-connector';
 import config from '../../config';
+import { publicProvider } from "wagmi/providers/public";
 import '@oceanprotocol/uploader-ui-lib/dist/index.es.css';
 const UploaderConnection = dynamic(() => import('@oceanprotocol/uploader-ui-lib').then((module) => module.UploaderConnection), { ssr: false });
 
 export default function Home() {
-
+  const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [polygon],
+    [publicProvider()]
+  );
+  /*
+  // Universal Wallet integration 
+  const magicLinkConnector = new DedicatedWalletConnector({
+    options: {
+      apiKey: process.env.NEXT_PUBLIC_MAGIC_API_KEY || 'pk_live_3EA01B119E287F11', //required
+      oauthOptions : {
+        providers: ['facebook', 'google', 'twitter'],
+        callbackUrl: 'https://your-callback-url.com', //optional
+      }
+    },
+  })
   const wagmiConfig = createConfig(
     getDefaultConfig({
       appName: 'Ocean Uploader UI',
       infuraId: process.env.INFURA_ID || '',
+      connectors: [ magicLinkConnector ],
       chains: [polygon],
       walletConnectProjectId: process.env.WALLET_CONNECT_PROJECT_ID || ''
-    })
+    }) 
   )
+    */
+  const config = createConfig({
+    autoConnect: true,
+    publicClient,
+    webSocketPublicClient,
+    connectors: [
+      new DedicatedWalletConnector({
+        chains,
+        options: {
+          apiKey:  process.env.NEXT_PUBLIC_MAGIC_API_KEY || 'pk_live_3EA01B119E287F11',
+          isDarkMode: true,
+          /* Make sure to enable OAuth options from magic dashboard */
+          oauthOptions: {
+            providers: ["google", "twitter", "github"],
+          },
+          magicSdkConfiguration: {
+            network: {
+              rpcUrl: 'https://rpc-mumbai.maticvigil.com/',
+              chainId: 137,
+            },
+          },
+        },
+      }),
+    ],
+  });
 
   return (
-    <WagmiConfig config={wagmiConfig}>
+    <WagmiConfig config={config}>
       <ConnectKitProvider>
         <Layout>
           <div className={styles.root}>
